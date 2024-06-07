5b6fb557c65004a2db21a55e0a2b65ad9ba45011
0x24a <96947247+0x24a@users.noreply.github.com>
2024-06-08 07:26:59 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,6 @@
 local ai_text = get("input-content")
-local ai_result = get("ai_results")
+local ai_result = get("ai-results")
+ai_result.set_content("Waiting...")
 get("send-ai").on_click(function()
   local response = fetch(
     url="https://skibidi.24a.fun/aigc/sparkdesk-v1.5?question="+ai_text.get_content(),
